c89669238e11f683e3895b584c6ec406e0506f1c
Hentschel <csav5997@uibk.ac.at>
2019-03-20 18:04:50 +0100

    Leaflet einbinden und Daten auslesen

---
+++ nz/main.js
@@ -0,0 +1,9 @@
+// Skript für Neuseelandreise
+
+const div = document.getElementById("map");
+const lat = div.getAttribute("data-lat");
+const lng = div.getAttribute("data-lng");
+const title = div.getAttribute("data-title");
+
+//console.log("Breite=",lat,"Länge=",lng,"Titel=",title);
+

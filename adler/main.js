b7fcc5622a3d7d0896704bbf4384161c534c04bb
Hentschel <csav5997@uibk.ac.at>
2019-04-03 16:30:56 +0200

    geolandbasemap einbauen

--- adler/main.js
+++ adler/main.js
@@ -15,8 +15,21 @@ const titel2 = div.getAttribute("data-title2");
 let karte = L.map("map");
 //console.log(karte);
 
+const kartenLayer = {
+    osm: L.tileLayer("http://{s}.tile.osm.org/{z}/{x}/{y}.png", {
+        subdomains: ["a", "b", "c"],
+        attribution: `Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>`
+    }),
+    geolandbasemap : L.tileLayer("https://{s}.wien.gv.at/basemap/geolandbasemap/normal/google3857/{z}/{y}/{x}.png", {
+        subdomains: ["maps", "maps1", "maps2", "maps2", "maps4"],
+        attribution: 'Datenquelle: <a href="https://www.basemap.at">basemap.at</a>'
+    })
+};
+
 //openstreetmap einbauen - s= server, z= zoom, x=laenge, y=breite
-L.tileLayer("http://{s}.tile.osm.org/{z}/{x}/{y}.png").addTo(karte);
+
+kartenLayer.osm.addTo(karte);
+//kartenLayer.geolandbasemap.addTo(karte);
 
 //Positionsmarker setzen
 let pin1 = L.marker(

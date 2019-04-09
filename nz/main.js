1fd7d9552d5ce78e723ab11a4a1436a005b517bd
susannehentschel <susanne.hentschel@student.uibk.ac.at>
2019-04-09 16:08:18 +0200

    Anpassung Karte

--- nz/main.js
+++ nz/main.js
@@ -5,20 +5,48 @@ const breite = div.getAttribute("data-lat");
 const laenge = div.getAttribute("data-lng");
 const titel = div.getAttribute("data-title");
 
-//console.log("Breite=",breite,"Länge=",laenge,"Titel=",titel);
-
 // Karte initialisieren
 let karte = L.map("map");
+
+//console.log("Breite=",breite,"Länge=",laenge,"Titel=",titel);
+
+const kartenLayer = {
+    osm: L.tileLayer("http://{s}.tile.osm.org/{z}/{x}/{y}.png", {
+        subdomains: ["a", "b", "c"],
+        attribution: `Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>`
+    }),
+    stamen_toner: L.tileLayer("https://stamen-tiles-{s}.a.ssl.fastly.net/toner/{z}/{x}/{y}.png", {
+        subdomains: ["a", "b", "c"],
+        attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, under <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a>. Data by <a href="http://openstreetmap.org">OpenStreetMap</a>, under <a href="http://www.openstreetmap.org/copyright">ODbL</a>.'
+    }),
+    stamen_terrain: L.tileLayer("http://stamen-tiles-{s}.a.ssl.fastly.net/terrain/{z}/{x}/{y}.jpg", {
+        subdomains: ["a", "b", "c"],
+        attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, under <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a>. Data by <a href="http://openstreetmap.org">OpenStreetMap</a>, under <a href="http://www.openstreetmap.org/copyright">ODbL</a>.'
+    }),
+    stamen_watercolor: L.tileLayer("http:////stamen-tiles-{s}.a.ssl.fastly.net/watercolor/{z}/{x}/{y}.jpg", {
+        subdomains: ["a", "b", "c"],
+        attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, under <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a>. Data by <a href="http://openstreetmap.org">OpenStreetMap</a>, under <a href="http://creativecommons.org/licenses/by-sa/3.0">CC BY SA</a>.'
+    }),  
+};
+
+    L.control.layers({
+        "OpenStreetMap": kartenLayer.osm,
+        "Stamen Toner" : kartenLayer.stamen_toner,
+        "Stamen Terrain" : kartenLayer.stamen_terrain,
+        "Stamen Watercolor" : kartenLayer.stamen_watercolor
+    }).addTo(karte);
+
+
 // console.log(karte);
+
+kartenLayer.osm.addTo(karte);
+//kartenLayer.geolandbasemap.addTo(karte);
 
 // auf Ausschnitt zoomen
 karte.setView(
     [breite, laenge],
     13
 );
-
-//openstreetmap einbauen - s= server, z= zoom, x=laenge, y=breite
-L.tileLayer("http://{s}.tile.osm.org/{z}/{x}/{y}.png").addTo(karte);
 
 //Positionsmarker setzen
 let pin = L.marker(
@@ -27,3 +55,13 @@ let pin = L.marker(
 
 //Popup zum Pin hängen
 pin.bindPopup(titel).openPopup();
+
+karte.addControl(new L.Control.Fullscreen());
+
+var hash = new L.Hash(karte);
+
+var coords = new L.Control.Coordinates();
+coords.addTo(karte);
+karte.on('click', function(e) {
+	coords.setCoordinates(e);
+});
